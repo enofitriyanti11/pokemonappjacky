a365fc1e08333fb077c91ff24fdec974cdfdfcc8
Jacky Andrazat <jackyandrazat@gmail.com>
2023-05-09 14:48:37 +0700

    Create Navbar dan List
    Membuat Navbar dan List Product

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+            <img src={require("../img/pokemonHero.jpeg")} className="max-w-sm rounded-lg shadow-2xl" />
+            <img src="img/pokemonHero.jpeg" className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+        </div>
+        </div>
+    </div>
+  )
+}
