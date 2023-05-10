4bbd00d7f73a28f5ea308bfff7dd4d602da53280
Jacky Andrazat <jackyandrazat@gmail.com>
2023-05-10 12:06:15 +0700

    Added Detail Pokemons
    Membuat Component Detail Pokemons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import ListPokemonts from './components/ListPokemonts';
 import Footer from './components/Footer';
 import HeroVideo from './components/HeroVideo';
-
+import DetailPokemons from './components/DetailPokemons';
 
 function App() {
   return (
