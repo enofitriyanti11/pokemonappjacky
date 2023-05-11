1f54a658b46c1bb20f0fab269087b1551f47ac6f
Jacky Andrazat <jackyandrazat@gmail.com>
2023-05-11 16:54:49 +0700

    Update ListPokemonts.js
    editing margin layout card list pokemons

--- src/components/ListPokemonts.js
+++ src/components/ListPokemonts.js
@@ -96,10 +96,10 @@ export default function ListPokemonts() {
   return (
     <div className="">
 
-      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div className="grid mb-8 grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {pokemons.map((pokemon) => (
           <a key={pokemon.id} href={pokemon.href} className="group relative">
-            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+            <div className="aspect-h-1 aspect-w-1 w-full my-8 overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
               <img
                 src={pokemon.imageSrc}
                 alt={pokemon.imageAlt}
