8ed54a7502c5ea4fe41040cca39d39c046785bcf
Jacky Andrazat <jackyandrazat@gmail.com>
2023-05-15 17:43:32 +0700

    Create Pagination.js
    Membuat Pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Pagination() {
+    return (
+        <div>
+            <div className="btn-group grid grid-cols-2">
+                <button className="btn btn-outline">Previous page</button>
+                <button className="btn btn-outline">Next</button>
+            </div>
+        </div>
+    )
+}
